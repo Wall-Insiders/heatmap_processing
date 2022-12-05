9050f7c36b1c360b384e2539857e31714dcb2bed
xdisna2 <aznthonyle45@gmail.com>
2022-12-04 22:40:20 -0800

    experimented with other heatmapping, however I have realized that a complete dataset is needed to represent the full dimensions of the wall

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ df = pd.read_csv('studs_found.csv')
 
 df = pd.DataFrame(df.pivot(index='x', columns='y', values='stud_found'))
 df = pd.DataFrame(df.fillna(0))
-#print(df.to_string())
-sns.heatmap(df)
+
+#plt.pcolormesh(df, vmax=1, vmin=0)
+#plt.colorbar()
+#plt.show()
+print(df.to_string())
+heat = sns.heatmap(df, linewidths=.5, annot=True)
+heat.set_xlim(0,71)
+heat.set_ylim(0,119)
+
+
 plt.show()
