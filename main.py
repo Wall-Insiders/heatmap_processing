02e4c232e7cf80ea7e6c06cbb466491c3ab3f373
xdisna2 <aznthonyle45@gmail.com>
2022-12-05 13:46:18 -0800

    Finalized heatmap test for possible stud finder inputs.

--- main.py
+++ main.py
@@ -8,20 +8,9 @@ import seaborn as sns
 import seaborn.objects as so
 
 # Create a heatmap of the studs_found data
+# Open the csv file as a Pandas dataframe 
+df = pd.read_csv('studs_found1.csv', index_col=False, dtype=int, header=0)
 
-# Open the csv file as a Pandas dataframe
-df = pd.read_csv('studs_found.csv')
-
-df = pd.DataFrame(df.pivot(index='x', columns='y', values='stud_found'))
-df = pd.DataFrame(df.fillna(0))
-
-#plt.pcolormesh(df, vmax=1, vmin=0)
-#plt.colorbar()
-#plt.show()
-print(df.to_string())
-heat = sns.heatmap(df, linewidths=.5, annot=True)
-heat.set_xlim(0,71)
-heat.set_ylim(0,119)
-
-
+# Display heatmap
+heat = sns.heatmap(df)
 plt.show()
