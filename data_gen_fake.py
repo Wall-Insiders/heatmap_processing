0890b5e007c9d6374225148ec6940fc32f1afe9c
xdisna2 <aznthonyle45@gmail.com>
2022-12-05 13:48:23 -0800

    Changed data methodology to store inputs. Rather than storing the coordinates and stud value in a 3 element array, have the array be the representation of the wall with the coordinate system being in the indexes of the array.

--- data_gen_fake.py
+++ data_gen_fake.py
@@ -3,39 +3,36 @@
 import csv
 import random
 import numpy as np
+import pandas as pd
 
-headers = ['x', 'y', 'stud_found']
-data = np.empty([8640,3], dtype=int)
-#print(data)
+# Initialize an empty Numpy array
+data = np.empty([120,72], dtype=int)
+
+# Set all data points not equal to 0 to 0
+data[data != 0] = 0
 
 # Generate random number from x(0,71) and y(0,119) (A regular 6 foot by 12 foot wall)
-x_values = list(range(0,71))
-y_values = list(range(0,119))
+x_values = list(range(72))
+y_values = list(range(120))
 
 # Random number generate 4 (x,y) coordinates with a z standard stud length
-for x in range(0, 16, 4):
+for x in range(4):
     temp_x = random.choice(x_values)
     temp_y = random.choice(y_values)
 
-    temp_data = [temp_x, temp_y, 1]
-    data[x] = temp_data
+    data[:, temp_x] = 1
+
 
 # Expand the 4 coordinates by (x+1,y) of 4 iterations (2x4 stud dimension)
-    for y in range(3):
-        temp_x += 1
-        data[x+y+1] = [temp_x, temp_y, 1]
+    for y in range(4):
+        data[:, temp_x+y] = 1
 
-#for data in range(16,8640,1):
-    #for x in range(0,71):
-
-
-#print(data)
 # Save coordinates to .csv file
 with open('studs_found1.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
 
     # write the header
-    writer.writerow(headers)
+    writer.writerow(x_values)
 
     # write multiple rows
     writer.writerows(data)
