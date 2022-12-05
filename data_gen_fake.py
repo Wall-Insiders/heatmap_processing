35afb1c6f8a34c897a910aefbdb4046a0059b356
xdisna2 <aznthonyle45@gmail.com>
2022-12-04 20:25:38 -0800

    Created file to generate csv files for testing heatmapping

---
+++ data_gen_fake.py
@@ -0,0 +1,6 @@
+# Creates a dataset for testing the heatmapping function of seaborn
+# Outputs to a .csv file to be accessed 
+
+# Random number generate 2 (x,y) coordinates with a z standard stud length
+# Expand the 2 coordinates by (x+1,y+1) of z iterations
+# Save coordinates to .csv file
