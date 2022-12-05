1fd5947ad7ae815e1de84ddd2945ffe271c9846c
xdisna2 <aznthonyle45@gmail.com>
2022-12-04 21:52:09 -0800

    Output a successful heatmap, however axis need to be fixed

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import seaborn.objects as so
 
+# Create a heatmap of the studs_found data
+
+# Open the csv file as a Pandas dataframe
+df = pd.read_csv('studs_found.csv')
+
+df = pd.DataFrame(df.pivot(index='x', columns='y', values='stud_found'))
+df = pd.DataFrame(df.fillna(0))
+#print(df.to_string())
+sns.heatmap(df)
+plt.show()
